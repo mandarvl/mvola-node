10b06dd8f62c7eef279cc88fa391435aea5b1a26
mandarvl <manda.ravalison@gmail.com>
2023-07-04 21:56:41 +0300

    remove axios fetch adapter

--- src/Client.ts
+++ src/Client.ts
@@ -1,7 +1,6 @@
 import AuthService from "./Auth";
 import TransactionService from "./Transaction";
 import axios, { AxiosRequestConfig } from "axios";
-import fetchAdapter from "@vespaiach/axios-fetch-adapter";
 
 export const SANDBOX_URL = "https://devapi.mvola.mg";
 export const PRODUCTION_URL = "https://api.mvola.mg";
@@ -10,11 +9,8 @@ class Client {
   transaction: TransactionService;
   auth: AuthService;
 
-  constructor(baseURL: string = SANDBOX_URL, useFetchAdapter: boolean = false) {
+  constructor(baseURL: string = SANDBOX_URL) {
     const options: AxiosRequestConfig = { baseURL };
-    if (useFetchAdapter) {
-      options.adapter = fetchAdapter;
-    }
     const client = axios.create(options);
     this.auth = new AuthService(axios.create(options));
     this.transaction = new TransactionService(client);
