7b35485c9a6b408f4ab194e3e31b0884b1a42207
mandarvl <manda.ravalison@gmail.com>
2024-09-17 11:14:29 +0300

    fix(auth): pass device uuid in params with backward compatibility

--- src/Auth.ts
+++ src/Auth.ts
@@ -6,12 +6,16 @@ import { AuthResponse } from "./Types";
 class AuthService extends Service {
   async generateToken(
     consumerKey: string,
-    consumerSecret: string
+    consumerSecret: string,
+    deviceUUID?: string,
   ): Promise<AuthResponse> {
-    const deviceUUID = process.env.DEVICE_UUID;
+    let scope = 'EXT_IN_MVOLA_SCOPE';
+    if (deviceUUID) {
+      scope += ` device_${deviceUUID}`;
+    }
     const params = qs.stringify({
       grant_type: "client_credentials",
-      scope: `EXT_INT_MVOLA_SCOPE device_${deviceUUID}`,
+      scope
     });
 
     const { data } = await this.client.post<AuthResponse>("/token", params, {
